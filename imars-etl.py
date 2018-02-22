61ba6f6a75fa566e345ce618b55969e5a82e2c4d
Tylar Murray <murray.tylar@gmail.com>
2018-02-22 16:17:17 +0000

    + std boilerplate

---
+++ imars-etl.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+""" cmd line interface definition for imars-etl """
+from argparse import ArgumentParser
+import logging
+
+import imars_etl
+
+assert __name__ == "__main__"
+# =========================================================================
+# === set up arguments
+# =========================================================================
+parser = ArgumentParser(description='Interface for IMaRS ETL operations')
+
+# === arguments for the main command
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="count",
+                    default=0
+)
+# other examples:
+# parser.add_argument("source", help="directory to copy from")
+# parser.add_argument('-l', '--log',
+#     help="desired filepath of log file",
+#     default="/var/opt/projectname/backup.log"
+# )
+# parser.add_argument('--rclonelog',
+#     help="desired path of rclone log file",
+#     default=None
+# )
+
+parser.set_defaults(func=imars_etl.extract)  # set default behavior if subcommand not given
+
+# === subcommands
+subparsers = parser.add_subparsers(
+    title='subcommands',
+    description='usage: `projectname $subcommand` ',
+    help='addtnl help for subcommands: `projectname $subcommand -h`'
+)
+
+parser_status = subparsers.add_parser('extract', help='download file from data warehouse')
+parser_status.set_defaults(func=imars_etl.extract)
+
+parser_status = subparsers.add_parser('load', help='upload file to data warehouse')
+parser_status.set_defaults(func=imars_etl.load)
+
+args = parser.parse_args()
+# =========================================================================
+# === set up logging behavior
+# =========================================================================
+if (args.verbose == 0):
+    logging.basicConfig(level=logging.WARNING)
+elif (args.verbose == 1):
+    logging.basicConfig(level=logging.INFO)
+else: #} (args.verbose == 2){
+    logging.basicConfig(level=logging.DEBUG)
+
+# === (optional) create custom logging format(s)
+# https://docs.python.org/3/library/logging.html#logrecord-attributes
+formatter = logging.Formatter(
+   '%(asctime)s|%(levelname)s\t|%(filename)s:%(lineno)s\t|%(message)s'
+)
+
+# === (optional) create handlers
+# https://docs.python.org/3/howto/logging.html#useful-handlers
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(_level)
+stream_handler.setFormatter(formatter)
+stream_handler.addFilter(DuplicateLogFilter())
+
+file_handler = RotatingFileHandler(
+   '/var/opt/imars_etl/imars_etl.log', maxBytes=1e6, backupCount=5
+)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+file_handler.addFilter(DuplicateLogFilter())
+
+# === add the handlers (if any) to the logger
+_handlers = [
+    stream_handler,
+    file_handler
+]
+
+logging.basicConfig(
+    handlers=_handlers,
+    level=logging.DEBUG  # this must be set to lowest of all levels used in handlers
+)
+# =========================================================================
+args.func(args)
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+""" imars-etl setup.py """
+
+from setuptools import setup
+import io
+
+import imars_etl
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md') #, 'CHANGES.txt')
+
+setup(name='imars_etl',
+    version=imars_etl.__version__,
+    description='Interface for IMaRS ETL operations',
+    long_description=long_description,
+    author='Tylar Murray',
+    author_email='code+imars_etl@tylar.info',
+    url='https://github.com/usf-imars/imars-etl',
+    tests_require=['nose'],
+    install_requires=[],
+    entry_points={  # sets up CLI (eg bash) commands
+        'console_scripts': [
+            'imars-etl = imars-etl',
+        ],
+    },
+    #cmdclass={'test': PyTest},  # custom build commands for test/lint/etc
+    packages=[  # modules that are added to python when this is installed
+        'imars_etl'
+    ]
+)
