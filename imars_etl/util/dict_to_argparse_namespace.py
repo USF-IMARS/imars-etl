6ae2ca4f11a0abc4c2412b3b54054c19fb2722ae
Tylar Murray <murray.tylar@gmail.com>
2018-03-06 19:03:48 +0000

    allow argparse.namespaces to pass through converter

--- imars_etl/util/dict_to_argparse_namespace.py
+++ imars_etl/util/dict_to_argparse_namespace.py
@@ -4,8 +4,11 @@ def dict_to_argparse_namespace(input_dict):
     """
     converts given argument dict into a proper argparse namespace object
     """
-    args = argparse.Namespace()
-    for key in input_dict:
-        val = input_dict[key]
-        setattr(args, key, val)
-    return args
+    if isinstance(input_dict, argparse.Namespace):
+        return input_dict
+    else:
+        args = argparse.Namespace()
+        for key in input_dict:
+            val = input_dict[key]
+            setattr(args, key, val)
+        return args
--- imars_etl/util/dict_to_argparse_namespace_test.py
+++ imars_etl/util/dict_to_argparse_namespace_test.py
@@ -10,6 +10,8 @@ except ImportError:
     # py3
     from unittest.mock import MagicMock
 
+import argparse
+
 # dependencies:
 from imars_etl.util import dict_to_argparse_namespace
 
@@ -19,7 +21,7 @@ class Test_dict_to_argparse_namespace(TestCase):
     #########################
     def test_dict_to_argparse_namespace_basic(self):
         """
-        test converting a simple dict
+        simple dict values accesible w/ attrib refs
         """
         test_dict={
             "a":"apple",
@@ -30,3 +32,24 @@ class Test_dict_to_argparse_namespace(TestCase):
         self.assertEqual(args.a,    'apple')
         self.assertEqual(args.beta, 'fish')
         self.assertEqual(args.c,    'cat')
+
+    def test_returns_namespace_obj(self):
+        """
+        return type is argparse.Namepace
+        """
+        test_dict={
+            "a":"apple",
+            "beta": "fish",
+            "c": "cat"
+        }
+        args = dict_to_argparse_namespace(test_dict)
+        self.assertTrue(isinstance(args, argparse.Namespace))
+
+    def test_argparse_passes_through(self):
+        """
+        passing a argparse.Namespace is fine
+        """
+        args = argparse.Namespace()
+        args.mytest = 'test'
+        args2 = dict_to_argparse_namespace(args)
+        self.assertEqual(args,args2)
