d37b541ca759924dd9205ac9b944314addbf4e91
Tylar Murray <murray.tylar@gmail.com>
2018-04-02 17:13:20 +0000

    raise i_err if not DNE so we can better see what happened

--- imars_etl/drivers/imars_objects/load.py
+++ imars_etl/drivers/imars_objects/load.py
@@ -2,6 +2,7 @@ import logging
 import sys
 import os
 import shutil
+import errno
 
 from imars_etl.drivers.imars_objects.satfilename import satfilename
 
@@ -17,7 +18,11 @@ def _load(args):
         try:
             shutil.move(args['filepath'], ul_target)
         except IOError as i_err:  # possible dir DNE
-            os.makedirs(os.path.dirname(ul_target))
-            shutil.move(args['filepath'], ul_target)
+            # ENOENT(2): file does not exist or missing dest parent dir
+            if i_err.errno != errno.ENOENT:
+                raise i_err
+            else:
+                os.makedirs(os.path.dirname(ul_target))
+                shutil.move(args['filepath'], ul_target)
 
     return ul_target
