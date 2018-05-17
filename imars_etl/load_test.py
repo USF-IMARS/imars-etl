8cc2a7071f05f90e17a0263015b753f4e35101d4
Tylar Murray <murray.tylar@gmail.com>
2018-05-17 18:29:49 +0000

    + test for validate args inserts json (failing)

--- imars_etl/load_test.py
+++ imars_etl/load_test.py
@@ -567,36 +567,40 @@ class Test_validate_args(TestCase):
                 "product_id": -1,
                 "product_type_name": "test_test_test",
                 "time":  "2018-02-26T13:00",
-                # "product_name": "myd01",
                 "datetime": datetime(2018,2,26,13),
-                "json": '{"status_id":1,"area_id":1}',
             },
             result_arg_dict
         )
 
-    # TODO:
-    # def test_validate_json_into_args(self):
-    #     """
-    #     validate_args sticks stuff from --json into args
-    #     """
-    #
-    #     test_args = parse_args([
-    #         '-vvv',
-    #         'load',
-    #         '--dry_run',
-    #         '-f', "/fake/path/file_w_date_2018.txt",
-    #         '-p', '-1',
-    #         '-t', "2018-02-26T13:00",
-    #         '-j', '{"status_id":1,"area_id":1}',
-    #     ])
-    #     self.assertSQLInsertKeyValuesMatch(
-    #         load(test_args),
-    #         ['status_id','date_time','area_id','product_id','filepath'],
-    #         [
-    #             '1',
-    #             '"2018-02-26T13:00"',
-    #             '1',
-    #             '-1',
-    #             '"/srv/imars-objects/test_test_test/simple_file_with_no_args.txt"'
-    #         ]
-    #     )
+    def test_validate_json_into_args(self):
+        """
+        validate_args sticks stuff from --json into args
+        """
+        from imars_etl.load import _validate_args
+        from imars_etl.cli import parse_args
+
+        test_args = parse_args([
+            '-vvv',
+            'load',
+            '--dry_run',
+            '-f', "/fake/path/file_w_date_2018.txt",
+            '-p', '-1',
+            '-t', "2018-02-26T13:00",
+            '-j', '{"status_id":1,"area_id":1}',
+        ])
+        result_arg_dict = _validate_args(test_args)
+        self.assertDictContainsSubset(
+            {
+                "verbose": 3,
+                "dry_run": True,
+                "filepath": "/fake/path/file_w_date_2018.txt",
+                "product_id": -1,
+                "product_type_name": "test_test_test",
+                "time":  "2018-02-26T13:00",
+                "datetime": datetime(2018,2,26,13),
+                "json": '{"status_id":1,"area_id":1}',
+                "status_id": 1,
+                "area_id": 1
+            },
+            result_arg_dict
+        )
