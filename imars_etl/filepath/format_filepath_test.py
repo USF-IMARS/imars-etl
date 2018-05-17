c659a5eff523cd7f053a1065304cc31927f1b43d
Tylar Murray <murray.tylar@gmail.com>
2018-05-17 14:38:06 +0000

    more tests

--- imars_etl/filepath/format_filepath_test.py
+++ imars_etl/filepath/format_filepath_test.py
@@ -26,3 +26,36 @@ class Test_format_filepath_template(TestCase):
             result,
             "/srv/imars-objects/modis_aqua_gom/myd01/A%Y%j.%H%M.hdf"
         )
+
+    def test_format_filename_w_product_id(self):
+        """ can create file format template with only product_id"""
+        result = _format_filepath_template(
+            None,
+            5,
+            forced_basename=None
+        )
+        self.assertEqual(
+            result,
+            "/srv/imars-objects/modis_aqua_gom/myd01/A%Y%j.%H%M.hdf"
+        )
+
+    def test_format_filename_w_prod_name(self):
+        """ can create file format template with only product_type_name"""
+        result = _format_filepath_template(
+            "myd01",
+            None,
+            forced_basename=None
+        )
+        self.assertEqual(
+            result,
+            "/srv/imars-objects/modis_aqua_gom/myd01/A%Y%j.%H%M.hdf"
+        )
+
+    def test_format_filename_w_nothing(self):
+        """ file format template with no product info raises err """
+        with self.assertRaises(ValueError):
+            result = _format_filepath_template(
+                None,
+                None,
+                forced_basename=None
+            )
