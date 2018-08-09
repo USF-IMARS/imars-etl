a7b85e14e3ae45f29737926b069100b9e99d8d73
Tylar Murray <murray.tylar@gmail.com>
2018-08-09 17:12:03 +0000

    + test demonstrates issue reading dt from sql str

--- imars_etl/Load/unify_metadata_test.py
+++ imars_etl/Load/unify_metadata_test.py
@@ -52,3 +52,35 @@ class Test_unify_metadata(TestCase):
             },
             result_arg_dict
         )
+
+    def Test_unify_identical_date_and_datestr(self):
+        """
+        unify_metadata can use date_time from sql string
+        """
+        import datetime
+        from imars_etl.Load.unify_metadata import unify_metadata
+        DT = datetime.datetime(2018, 8, 8, 19, 25)
+        TIMESTR = '2018-08-08T19:25:00.000000'
+        result_arg_dict = unify_metadata(
+            verbose=3,
+            dry_run=True,
+            product_id=35,
+            nohash=True,
+            sql=(
+                "product_id=35 AND area_id=1 "
+                "AND date_time='2018-08-08 19:25:00'"
+            ),
+            time=TIMESTR,
+            filepath='/processing_modis_aqua_pass_gom_20180808T192500_l2_file',
+            product_type_name='myd0_otis_l2',
+            load_format='{dag_id}_%Y%m%dT%H%M%S_{tag}',
+            date_time=DT,
+            json='{"status_id":3,"area_short_name":"gom"}'
+        )
+        self.assertDictContainsSubset(
+            {
+                "date_time": DT,
+                "time": TIMESTR,
+            },
+            result_arg_dict
+        )
