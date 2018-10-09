4e1a5aaa2e4158ce77ab36c8923fa619db138075
Tylar Murray <murray.tylar@gmail.com>
2018-10-09 22:39:56 +0000

    split validation out from sql get

--- imars_etl/util/get_sql_result.py
+++ imars_etl/util/get_sql_result.py
@@ -39,28 +39,41 @@ def get_sql_result(
             else:
                 result = cursor.fetchmany(2)
 
-            if (check_result and not result):
-                raise NoMetadataMatchException(
-                    "Zero files found matching given metadata."
-                )
-                # exit(EXIT_STATUS.NO_MATCHING_FILES)
-            elif (check_result and len(result) > 1):
-                # TODO: request more info from user?
-                logger.error(result)
-                raise TooManyMetadataMatchesException(
-                    "Too many results found matching given metadata."
-                )
-                # exit(EXIT_STATUS.MULTIPLE_MATCH)
-            else:
-                if should_commit:
-                    connection.commit()
-                try:
-                    return result[0]
-                except IndexError:
-                    if check_result:
-                        raise
-                    else:  # we don't care about the error
-                        logger.info("no result from sql request")
-                        return result
+            if check_result is True:
+                validate_sql_result(result)
+
+            if should_commit:
+                connection.commit()
+
+            try:
+                return result[0]
+            except IndexError:
+                if check_result:
+                    raise
+                else:  # we don't care about the error
+                    logger.info("no result from sql request")
+                    return result
     finally:
         connection.close()
+
+
+def validate_sql_result(result):
+    logger = logging.getLogger("{}.{}".format(
+        __name__,
+        sys._getframe().f_code.co_name)
+    )
+
+    if (not result):
+        raise NoMetadataMatchException(
+            "Zero files found matching given metadata."
+        )
+        # exit(EXIT_STATUS.NO_MATCHING_FILES)
+    elif (len(result) > 1):
+        # TODO: request more info from user?
+        logger.error(result)
+        raise TooManyMetadataMatchesException(
+            "Too many results found matching given metadata."
+        )
+        # exit(EXIT_STATUS.MULTIPLE_MATCH)
+    else:
+        return
