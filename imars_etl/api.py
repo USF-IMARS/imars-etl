afbabc6d79409f99e99d9bf259c3c3cea3b0d594
Tylar Murray <murray.tylar@gmail.com>
2018-10-10 16:02:50 +0000

    rm ref to (prev del) setup_conn

--- imars_etl/api.py
+++ imars_etl/api.py
@@ -7,7 +7,3 @@ from imars_etl.Load.Load import load  # noqa F401
 from imars_etl.extract import extract  # noqa F401
 from imars_etl.id_lookup import id_lookup  # noqa F401
 from imars_etl.select import select  # noqa F401
-
-from imars_etl.setup_connections import setup_connections
-
-setup_connections()
