f6355940349a7c33a9ae07f367024b4f01ce9657
Tylar Murray <murray.tylar@gmail.com>
2019-01-03 20:33:04 +0000

    make failed test specific to problem fn.

--- imars_etl/Load/unify_metadata_test.py
+++ imars_etl/Load/unify_metadata_test.py
@@ -78,3 +78,35 @@ class Test_unify_metadata(TestCase):
             "time": TIMESTR,
         }
         assert expected_subset.items() <= result_arg_dict.items()
+
+
+class Test_sql_str_to_dict(TestCase):
+    def test_parse_dt_w_airflow_tz(self):
+        """
+        sql_str_to_dict converts airflow+timezone dt string to datetime
+
+        Adapted from failing s3_chloro_a task on 2019-01:
+        ```python
+        DATETIME = "2018-06-22T16:25:25+00:00"
+        FNAME = "processing_s3_chloro_a__florida_20180622T162525000000_l2_file"
+        args = {
+            'filepath': '/srv/imars-objects/airflow_tmp/'+FNAME,
+            'json': '{"area_short_name":"florida"}',
+            'sql': "product_id=49 AND area_id=12 AND date_time='"+DATETIME+"'"
+        }
+        imars_etl.load(**args)
+        >>> Traceback (most recent call last):
+        >>>    ...
+        >>>  File "/opt/imars_etl/imars_etl/Load/metadata_constraints.py", l12
+        >>>    ('time', ['date_time'], lambda dt: dt.strftime(ISO_8601_FMT)),
+        >>> AttributeError: 'str' object has no attribute 'strftime'
+        ```
+        """
+        import datetime
+        from imars_etl.Load.unify_metadata import sql_str_to_dict
+        DATE_TIME = "2018-06-22T16:25:25+00:00"
+        result_arg_dict = sql_str_to_dict(
+            "date_time='"+DATE_TIME+"'"
+        )
+
+        assert isinstance(result_arg_dict['date_time'], datetime.datetime)
