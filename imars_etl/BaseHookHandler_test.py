d4e2240de3641cf80563684240d34bf6ca09eab2
7yl4r <git@tylar.info>
2019-10-28 13:25:09 -0400

    + test reproducing observed behavior

---
+++ imars_etl/BaseHookHandler_test.py
@@ -0,0 +1,33 @@
+"""
+"""
+
+# std modules:
+from unittest import TestCase
+from unittest.mock import patch
+
+from airflow.exceptions import AirflowException
+
+from imars_etl.exceptions.NoMetadataMatchException \
+    import NoMetadataMatchException
+from imars_etl.exceptions.TooManyMetadataMatchesException \
+    import TooManyMetadataMatchesException
+
+
+class Test_get_hook(TestCase):
+    # mock patch settings.Session().query() to throw AirflowException before
+    #   conn is initialized.
+    @patch(
+        "airflow.settings.Session",
+        side_effect=AirflowException("test db exception")
+    )
+    def test_get_supplemental_hook(self, mocked_settings_session):
+        """
+        Gets a supplemental (non-airflow-supported) hook with early db error.
+
+        Reproduces the following error fixed in v0.14.1
+        "UnboundLocalError: local variable 'conn' referenced before assignment"
+        """
+        from imars_etl.BaseHookHandler import _get_hook
+        _get_hook(
+            "imars_object_store"
+        )
