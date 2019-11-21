431db72a4e7ecc2374e129b43bd2e1dfdf071e1b
7yl4r <git@tylar.info>
2019-11-21 10:57:30 -0500

    v0.14.2

--- imars_etl/__init__.py
+++ imars_etl/__init__.py
@@ -34,4 +34,4 @@ except Exception as err:
 
 from imars_etl.api import *  # noqa F401
 
-__version__ = "0.14.1"  # NOTE: this should match version in setup.py
+__version__ = "0.14.2"  # NOTE: this should match version in setup.py
