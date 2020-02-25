aa1d302c1a55e36a04a561cda5f689b55ac02488
7yl4r <git@tylar.info>
2020-02-25 11:46:49 -0500

    rm code ported to filepanther

--- imars_etl/filepath/formatter_hardcoded/get_product_id.py
+++
@@ -1,6 +0,0 @@
-from filepanther.formatter_hardcoded.data import data
-
-
-def get_product_id(product_type_name):
-    """Get product id from given name"""
-    return data[product_type_name]["imars_object_format"]["product_id"]
