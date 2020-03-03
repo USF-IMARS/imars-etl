7d759047776f399d555632939136b214edfe6054
Tylar <murray.tylar@gmail.com>
2020-03-03 13:17:27 -0500

    Update wv_ingest.sh

--- examples/load/wv_ingest.sh
+++ examples/load/wv_ingest.sh
@@ -1,8 +1,21 @@
 #!/bin/bash 
 # Loads all wv2 ntf files from pgc ingest.
 
-find /srv/imars-objects/tpa_gs/migration/alab/ -name 'WV02*M1BS*.ntf' \
+
+find /srv/imars-objects/tpa_gs/migration/monroe/ -name 'WV02*M1BS*.ntf' \
 	| xargs -n 1 imars-etl load \
-		--sql 'product_id=11 AND area_id=19 AND provenance="tylar_manual_ingest-2020-02"' \
+		--duplicates_ok
+		--sql 'product_id=11 AND area_id=9 AND provenance="tylar_manual_ingest-2020-03"' \
 		--metadata_file_driver wv2_xml  \
-		--metadata_file "{directory}/{basename}.xml"
+		--metadata_file "{directory}/{basename}.xml" \
+		--load_format 'WV02_%Y%m%d%H%M%S_{unknown_number}_%y%b%d%H%M%S-M1BS-{idNumber}_P{passNumber}.ntf' \
+		--no_load
+		
+find /srv/imars-objects/tpa_gs/migration/monroe/ -name 'WV02*M1BS*.xml' \
+	| xargs -n 1 imars-etl load \
+		--duplicates_ok
+		--sql 'product_id=14 AND area_id=9 AND provenance="tylar_manual_ingest-2020-03"' \
+		--metadata_file_driver wv2_xml  \
+		--metadata_file "{directory}/{basename}.xml" \
+		--load_format 'WV02_%Y%m%d%H%M%S_{unknown_number}_%y%b%d%H%M%S-M1BS-{idNumber}_P{passNumber}.ntf' \
+		--no_load
