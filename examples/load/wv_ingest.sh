9cd3c3a60287d6c1fd07a66a1002ca8a9d9cff9e
Tylar <murray.tylar@gmail.com>
2020-03-03 15:00:59 -0500

    fix filepaths & + missing \

--- examples/load/wv_ingest.sh
+++ examples/load/wv_ingest.sh
@@ -2,18 +2,18 @@
 # Loads all wv2 ntf files from pgc ingest in the monroe region (id=9).
 
 
-find /srv/imars-objects/tpa_gs/migration/monroe/ -name 'WV02*M1BS*.ntf' \
+find /srv/imars-objects/monroe/worldview -name 'WV02*M1BS*.ntf' \
 	| xargs -n 1 imars-etl load \
-		--duplicates_ok
+		--duplicates_ok \
 		--sql 'product_id=11 AND area_id=9 AND provenance="tylar_manual_ingest-2020-03"' \
 		--metadata_file_driver wv2_xml  \
 		--metadata_file "{directory}/{basename}.xml" \
 		--load_format 'WV02_%Y%m%d%H%M%S_{unknown_number}_%y%b%d%H%M%S-M1BS-{idNumber}_P{passNumber}.ntf' \
 		--no_load
 		
-find /srv/imars-objects/tpa_gs/migration/monroe/ -name 'WV02*M1BS*.xml' \
+find /srv/imars-objects/monroe/worldview -name 'WV02*M1BS*.xml' \
 	| xargs -n 1 imars-etl load \
-		--duplicates_ok
+		--duplicates_ok \
 		--sql 'product_id=14 AND area_id=9 AND provenance="tylar_manual_ingest-2020-03"' \
 		--metadata_file_driver wv2_xml  \
 		--metadata_file "{directory}/{basename}.xml" \
