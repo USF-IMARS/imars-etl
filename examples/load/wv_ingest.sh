0ce88a8963c4a63726c64c597f69e38b879200e6
Tylar <murray.tylar@gmail.com>
2020-03-04 14:54:52 -0500

    fix file extension for xml

--- examples/load/wv_ingest.sh
+++ examples/load/wv_ingest.sh
@@ -17,5 +17,5 @@ find /srv/imars-objects/monroe/worldview -name 'WV02*M1BS*.xml' \
 		--sql 'product_id=14 AND area_id=9 AND provenance="tylar_manual_ingest-2020-03"' \
 		--metadata_file_driver wv2_xml  \
 		--metadata_file "{directory}/{basename}.xml" \
-		--load_format 'WV02_%Y%m%d%H%M%S_{unknown_number}_%y%b%d%H%M%S-M1BS-{idNumber}_P{passNumber}.ntf' \
+		--load_format 'WV02_%Y%m%d%H%M%S_{unknown_number}_%y%b%d%H%M%S-M1BS-{idNumber}_P{passNumber}.xml' \
 		--no_load
